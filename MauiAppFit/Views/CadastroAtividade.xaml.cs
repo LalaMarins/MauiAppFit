6154eaea1e1c9bd5da630e55154b1dddc8193d00
0200832311016 <0200832311016@LAB03-14>
2025-10-31 22:00:54 -0300

    fim aula 31/10

--- MauiAppFit/Views/CadastroAtividade.xaml.cs
+++ MauiAppFit/Views/CadastroAtividade.xaml.cs
@@ -1,4 +1,6 @@
-using 
+using MauiAppFit.Models;
+using MauiAppFit.ViewModels;
+
 namespace MauiAppFit.Views;
 
 public partial class CadastroAtividade : ContentPage
@@ -6,5 +8,17 @@ public partial class CadastroAtividade : ContentPage
 	public CadastroAtividade()
 	{
 		InitializeComponent();
+
+		BindingContext = new CadastroAtividadeViewModel();
 	}
+    protected override void OnAppearing()
+    {
+        var vm = (CadastroAtividadeViewModel)BindingContext;
+
+		if (vm.Id == 0)
+		{
+			vm.NovaAtividade.Execute(null);
+		}
+		base.OnAppearing();
+    }
 }
